cf0ce74151105b59c0c54ddc42e4c92e194b3c1e
aliusman3 <48241839+aliusman3@users.noreply.github.com>
2020-11-17 23:40:29 +0500

    refactor: Removed Landing page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, NavLink, Redirect } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 import { useSelector } from 'react-redux';
 import { selectFavoritesLength } from './ducks/favorites';
 import theme from './theme';
-import { RootLayout, NavBar, Landing } from './styled';
-import SearchBar from './components/SearchBar';
+import { RootLayout, NavBar } from './styled';
 import SearchPage from './components/SearchPage';
 import FavoritesPage from './components/FavoritesPage';
 
 const Link = styled(NavLink)`
   text-decoration: underline;
   color: white;
-  margin-right: 10px
+  margin-right: 10px;
+  font-size: 1rem;
 `;
 
 function App() {
@@ -25,14 +25,9 @@ function App() {
             <Link to="/favorites" >Favorites({favoritesLength})</Link>
           </NavBar>
           <Switch>
-            <Route exact path='/' render={() => (
-              <Landing>
-                <h1 style={{ gridArea: 'landing-text', alignSelf: 'center' }}>Search for your favorite artists</h1>
-                <SearchBar />
-              </Landing>
-            )} />
             <Route path='/search' component={SearchPage}/>
             <Route path='/favorites' component={FavoritesPage} />
+            <Redirect to='/search' />
           </Switch>
         </RootLayout>
       </ThemeProvider>
